23b249e68f4f8a08f6bfdb6b933eb8f94f88c032
Hong Jiang <jianghong@xd.com>
2021-06-08 21:11:38 +0800

    wip: products section

--- components/Products.jsx
+++ components/Products.jsx
@@ -3,7 +3,17 @@ import styles from "../styles/Products.module.css";
 export default function Products() {
   return (
     <div className={styles.productsSection}>
-      <h1>快速发展的产品服务</h1>
+      <div className={styles.contents}>
+        <h1>快速发展的产品服务</h1>
+        <div className={styles.lowerContainerTDS}>
+          <div className={styles.sausagemanContainer}>
+            <div></div>
+            <div></div>
+            <img src="/images/sausageman-screen.png" />
+          </div>
+          <div></div>
+        </div>
+      </div>
     </div>
   );
 }
