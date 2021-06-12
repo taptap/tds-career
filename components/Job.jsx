c78bd7d11bb9aaf8ca7d853e72284113b87d18ac
Hong Jiang <lazyseq@gmail.com>
2021-06-13 01:01:58 +0800

    implement jobs section

--- components/Job.jsx
+++ components/Job.jsx
@@ -1,41 +1,34 @@
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Job.module.css";
 
 export default function Job({ job }) {
   return (
-    <>
-      <div className={styles.grid}>
-        <div className={styles.card}>
-          <h2>要求</h2>
-          <ul>
-            {job.requirements.map((r, i) => (
-              <li key={i}>{r}</li>
-            ))}
-          </ul>
-          {job.bonuses && (
-            <>
-              <h2>加分项</h2>
-              <ul>
-                {job.bonuses.map((b, i) => (
-                  <li key={i}>{b}</li>
-                ))}
-              </ul>
-            </>
-          )}
-        </div>
-        <div className={styles.card}>
-          <h2>职责</h2>
-          <ul>
-            {job.responsibilities.map((r, i) => (
-              <li key={i}>{r}</li>
-            ))}
-          </ul>
-          <h2>申请方式</h2>
-          <p>
-            请将简历发送至{" "}
-            <a href="mailto:tds-jobs@taptap.com">tds-jobs@taptap.com</a>
-          </p>
-        </div>
+    <div className={styles.job}>
+      <div className={styles.requirements}>
+        <h2>要求</h2>
+        <ul>
+          {job.requirements.map((r, i) => (
+            <li key={i}>{r}</li>
+          ))}
+        </ul>
+        {job.bonuses && (
+          <>
+            <h2>加分项</h2>
+            <ul>
+              {job.bonuses.map((b, i) => (
+                <li key={i}>{b}</li>
+              ))}
+            </ul>
+          </>
+        )}
       </div>
-    </>
+      <div className={styles.responsibilities}>
+        <h2>职责</h2>
+        <ul>
+          {job.responsibilities.map((r, i) => (
+            <li key={i}>{r}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
   );
 }
--- components/JobSelection.jsx
+++ components/JobSelection.jsx
@@ -12,15 +12,14 @@ export default function JobSelection({ job, allJobs }) {
     });
   };
   return (
-    <>
-      <div className={styles.jobSelection}>
+    <div className={styles.jobsSection}>
+      <div className={styles.contents}>
         <h1>探索你感兴趣的职位</h1>
         <p>
           目前我们在招聘以下这些职位，如果其中有适合你的，可提交申请至{" "}
           <a href="https://jobs.leanapp.cn">jobs.leanapp.cn</a>。
         </p>
-        <div>
-          选择职位：
+        <div className={styles.selection}>
           <select onChange={change} value={job.slug}>
             {allJobs.map((j) => (
               <option key={j.slug} value={j.slug}>
@@ -28,9 +27,10 @@ export default function JobSelection({ job, allJobs }) {
               </option>
             ))}
           </select>
+          <a href="mailto:tds-jobs@taptap.com">提交简历</a>
         </div>
+        {job && <Job job={job} />}
       </div>
-      {job && <Job job={job} />}
-    </>
+    </div>
   );
 }
