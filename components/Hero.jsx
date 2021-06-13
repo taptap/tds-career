a6d8d9bfbb88a0c163539d8c4b8ca254d9f4dd2a
Hong Jiang <lazyseq@gmail.com>
2021-06-13 23:09:39 +0800

    use png for Tarara

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -28,7 +28,7 @@ export default function Hero() {
           TapTap 开发者服务致力于为游戏开发者提供一流的工具、平台和服务
         </div>
         <div className={styles.joinButton}>加入我们</div>
-        <img className={styles.tarara} alt="Tarara" src="/images/tarara.svg" />
+        <img className={styles.tarara} alt="Tarara" src="/images/tarara.png" />
       </div>
     </div>
   );
