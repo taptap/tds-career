25aa350bcdfb7e95589cba199f5d0594475eaafe
Hong Jiang <lazyseq@gmail.com>
2021-06-13 22:13:17 +0800

    make top nav full-width

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -3,18 +3,18 @@ import styles from "../styles/Hero.module.css";
 export default function Hero() {
   return (
     <div className={styles.heroSection}>
+      <div className={styles.topNav}>
+        <a href="https://www.taptap.com">
+          <img
+            className={styles.topNavLogo}
+            alt="TapTap"
+            src="/images/taptap.svg"
+          />
+        </a>
+        <div className={styles.navDivider} />
+        <a href="https://developer.taptap.com">开发者服务</a>
+      </div>
       <div className={styles.contents}>
-        <div className={styles.topNav}>
-          <a href="https://www.taptap.com">
-            <img
-              className={styles.topNavLogo}
-              alt="TapTap"
-              src="/images/taptap.svg"
-            />
-          </a>
-          <div className={styles.navDivider} />
-          <a href="https://developer.taptap.com">开发者服务</a>
-        </div>
         <div className={styles.title}>
           加入
           <img
