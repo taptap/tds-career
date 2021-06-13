a3ab5b9c5b8307327e9d2aec83267276addaa25c
Hong Jiang <lazyseq@gmail.com>
2021-06-13 21:06:08 +0800

    add text to photo slideshow

--- components/Environment.jsx
+++ components/Environment.jsx
@@ -12,6 +12,13 @@ const fadeImages = [
 export default function Environment() {
   return (
     <div className="slide-container">
+      <div className={styles.text}>
+        <h1>富有创造力的工作环境</h1>
+        <h2>
+          上海办公室 位于静安区大宁中心广场 北京办公室 位于朝阳区核心商圈，临近
+          6 号线、17 号线
+        </h2>
+      </div>
       <Fade
         indicators={true}
         arrows={false}
