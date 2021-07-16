c79b8cc794d263063b0d8ff47441a613ec7df4f7
Hong Jiang <jianghong@xd.com>
2021-07-16 17:58:43 +0800

    add twitter card

--- components/Template.jsx
+++ components/Template.jsx
@@ -10,8 +10,27 @@ export default function Template({ children }) {
   return (
     <div className={styles.container}>
       <Head>
-        <title>成为 TDS 的一员</title>
-        <meta name="description" content="TapTap 的工作机会" />
+        <title>加入 TapTap Developer Services 团队</title>
+        <meta name="description" content="TDS 多个职位火热招聘中" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@gamesTapTap" />
+        <meta name="twitter:creator" content="@jiangh" />
+        <meta
+          name="twitter:title"
+          content="加入 TapTap Developer Services 团队"
+        />
+        <meta name="twitter:description" content="TDS 多个职位火热招聘中" />
+        <meta
+          name="twitter:image"
+          content="https://career.taptap.dev/images/card.png"
+        />
+        <meta name="og:type" content="summary_large_image" />
+        <meta name="og:title" content="加入 TapTap Developer Services 团队" />
+        <meta name="og:description" content="TDS 多个职位火热招聘中" />
+        <meta
+          name="og:image"
+          content="https://career.taptap.dev/images/card.png"
+        />
         <link
           rel="icon"
           href="//assets.tapimg.com/developer-center-v2/favicon.ico"
