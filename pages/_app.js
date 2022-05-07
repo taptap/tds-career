8ca23e10f46562eb73dacea36d05d00bf5535f9f
Hong Jiang <j@1byte.io>
2022-05-07 14:53:10 +0800

    remove unused files

--- pages/_app.js
+++
@@ -1,9 +0,0 @@
-import "../styles/globals.css";
-import "react-slideshow-image/dist/styles.css";
-import "../styles/slideshow.css";
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
